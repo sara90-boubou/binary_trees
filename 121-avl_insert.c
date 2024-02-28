444753f01825f9d5128d6291a80ad93735ba2e09
sara90-boubou <saraboubou4@gmail.com>
2024-02-28 13:26:43 -0800

    a function that inserts a value in an AVL Tree

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+size_t height(const binary_tree_t *tree);
+int balance(const binary_tree_t *tree);
+avl_t *avl_insert_recursive(avl_t **tree, avl_t *parent,
+		avl_t **new, int value);
+avl_t *avl_insert(avl_t **tree, int value);
+
 /**
  * height - Measures the height of a binary tree.
  * @tree: A pointer to the root node of the tree to measure the height.
@@ -12,8 +18,8 @@ size_t height(const binary_tree_t *tree)
 	{
 		size_t l = 0, r = 0;
 
-		l = tree->left ? 1 + height(tree->left) : 0;
-		r = tree->right ? 1 + height(tree->right) : 0;
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 		return ((l > r) ? l : r);
 	}
 	return (0);
@@ -63,16 +69,16 @@ avl_t *avl_insert_recursive(avl_t **tree, avl_t *parent,
 		return (*tree);
 
 	bfactor = balance(*tree);
-	if (bfactor > 1 && value < (*tree)->left->n)
+	if (bfactor > 1 && (*tree)->left->n > value)
 		*tree = binary_tree_rotate_right(*tree);
-	else if (bfactor < -1 && value > (*tree)->right->n)
+	else if (bfactor < -1 && (*tree)->right->n < value)
 		*tree = binary_tree_rotate_left(*tree);
-	else if (bfactor > 1 && value > (*tree)->left->n)
+	else if (bfactor > 1 && (*tree)->left->n < value)
 	{
 		(*tree)->left = binary_tree_rotate_left((*tree)->left);
 		*tree = binary_tree_rotate_right(*tree);
 	}
-	else if (bfactor < -1 && value < (*tree)->right->n)
+	else if (bfactor < -1 && (*tree)->right->n > value)
 	{
 		(*tree)->right = binary_tree_rotate_right((*tree)->right);
 		*tree = binary_tree_rotate_left(*tree);
